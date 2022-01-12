feb346480b4d8d26243081c7a3ae1e75f77a67d3
Laurens <laurensmeeus93@gmail.com>
2022-01-12 17:21:40 +0100

    cleanup code and add dev python environment

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -4,14 +4,14 @@ import json
 
 from src.cleaning.cleaning_trafilatura import get_json_trafilatura
 
-MEDIA_ROOT='tests/test_files'
+MEDIA_ROOT = 'tests/test_files'
+
 
 def test_get_json_trafilatura():
-    
-    html=open( os.path.join( MEDIA_ROOT, "test.html" ) ).read()
-    
-    true_json=json.loads( open( os.path.join( MEDIA_ROOT, "json_trafilatura.json" ) ).read() )
-        
-    json_trafilatura=get_json_trafilatura( html , target_language='en' )
-    
+    html = open(os.path.join(MEDIA_ROOT, "test.html")).read()
+
+    true_json = json.loads(open(os.path.join(MEDIA_ROOT, "json_trafilatura.json")).read())
+
+    json_trafilatura = get_json_trafilatura(html, target_language='en')
+
     assert true_json == json_trafilatura
